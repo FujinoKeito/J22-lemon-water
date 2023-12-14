3d2fbfb8e1266bb4bbf04fed399ef89d92675498
Uehara Takuma <kd1297552@st.kobedenshi.ac.jp>
2023-12-14 14:41:06 +0900

    動物・マイカラ―診断

--- javascript/testscript.js
+++ javascript/testscript.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById('question-container2').style.display = 'none';
     document.getElementById('result-container2').style.display = 'none';
     document.getElementById('question-container3').style.display = 'none';
- 
+    document.getElementById('result-container3').style.display = 'none';
 });
 
 const questions1 = [ //性格診断
@@ -23,11 +23,11 @@ const questions1 = [ //性格診断
 ];
 
 const questions2 = [//動物診断
-    "Q1. 他人との関わり方で当てはまるのは？ 1.自分のペースで過ごすことが好き 2.積極的に他人と交流することが好き",
+    "Q1. 他人との関わり方で当てはまるのは？\n 1.自分のペースで過ごすことが好き 2.積極的に他人と交流することが好き",
     "Q2. 好奇心の強さで当てはまるのは？ 1.慎重でなかなか新しいことに挑戦しない 2.新しいものに興味津々 ",
     "Q3. 複数人で議論するときに当てはまるのは？ 1.自分のこだわりを曲げない 2.周りの意見に合わせる",
     "Q4. 失敗してしまったときに当てはまるのは？ 1.あのときああしてればよかったと後悔 2.終わったことは気にせず切り替える",
-    "Q5. 休日の過ごし方は？？ 1.家でまったり 2.外でアクティブに遊ぶ",
+    "Q5. 休日の過ごし方は？ 1.家でまったり 2.外でアクティブに遊ぶ",
     "Q6. やらなければならないことがあるときに当てはまるのは？ 1.計画を立ててコツコツと努力する 2.少ない期間に一気にやり遂げる",
     "Q7. 好みの環境は？ 1.静かな場所や自分だけの空間 2.賑やかな環境で活動的",
     "Q8. ストレスを感じた時はどうする？ 1.一人で抱え込む 2.すぐ周りに相談",
@@ -36,8 +36,16 @@ const questions2 = [//動物診断
     // ここに質問を追加してください
 ];
 const questions3 = [ //マイカラー診断
-    "持っている情報や知識、時間はできるだけ自分のために使いたい",
-    "用心深く、いろいろなことを気にかけたり心配したりする",
+"Q1. 波風の立ちそうな状況からは身を引こうとする",
+"Q2. 得た知識や情報をよく考えて分析することが好きだ",
+"Q3. 持っている情報や知識、時間はできるだけ自分のために使いたい",
+"Q4. 任されたことは忠実にその責任を果たそうとする",
+"Q5. 休日の過ごし方は？？ 1.家でまったり 2.外でアクティブに遊ぶ",
+"Q6. ゆったりとしたペースが好きで、慌てることは少ない",
+"Q7. 他人の目や思惑はあまり気にせずに行動する",
+"Q8. 決められているルールや約束はきちんと守る",
+"Q9. 感情に振り回されることは少ない",
+"Q10. 人とは分けへだてなく公平公正に接する",
     // ここに質問を追加してください
 ];
 
@@ -126,10 +134,10 @@ function showQuestion3() { //マイカラー診断
     console.log(currentQuestionIndex3);
     console.log(scores3);
     document.getElementById('question-mode3').textContent = 'マイカラー診断';
-    const questionContainer3 = document.getElementById('question-container3');
-    const questionText3= document.getElementById('question-text3');
-    questionText3.textContent = questions3[currentQuestionIndex3];
-    questionContainer3.style.display = 'block';
+    const questionContainer = document.getElementById('question-container3');
+    const questionText= document.getElementById('question-text3');
+    questionText.textContent = questions3[currentQuestionIndex3];
+    questionContainer.style.display = 'block';
 
     //ラジオボタンの入力履歴を設定
     const answerIndex = currentQuestionIndex3;
@@ -141,8 +149,8 @@ function showQuestion3() { //マイカラー診断
     }else if(scores3[answerIndex] === 2){
         radioNodeList = answer_radio.elements[2].checked = true;
     }else{
-        const answerForm3 = document.getElementById('answer-form3');
-        answerForm3.reset();
+        const answerForm = document.getElementById('answer-form3');
+        answerForm.reset();
     }
 
     if (currentQuestionIndex3 === 0) { //1問目の時だけ戻るボタンを非表示にする
@@ -266,24 +274,24 @@ function showResult2() {
 
 
     // 結果を比較して表示を変更
-    let resultText = "";
+    let resultText2 = "";
     if (totalScore <= 12) {
-        resultText = "犬";
+        resultText2 = "犬";
     } else if (totalScore <= 16) {
-        resultText = "馬";
+        resultText2 = "馬";
     } else if (totalScore <= 20) {
-        resultText = "ライオン";
+        resultText2 = "ライオン";
     } else if (totalScore <= 24) {
-        resultText = "ゾウ";
+        resultText2 = "ゾウ";
     } else if (totalScore <= 27) {
-        resultText = "猫";
+        resultText2 = "猫";
     } else if (totalScore <= 30) {
-        resultText = "コアラ";
+        resultText2 = "コアラ";
     } else {
-        resultText = "その他"; // それ以外の場合にも対応する場合
+        resultText2 = "その他"; // それ以外の場合にも対応する場合
     }
 
-    resultTextElement.textContent = resultText;
+    resultTextElement.textContent = resultText2;
 
     // 質問画面を非表示
     document.getElementById('question-container2').style.display = 'none';
@@ -295,4 +303,46 @@ function showResult2() {
 function calculateTotalScore2() {
     // 各質問の得点を合算する
     return scores2.reduce((total, score) => total + score, 0);
+
+}
+function showResult3() {
+    // 結果画面の表示
+    const totalScore = calculateTotalScore3();  // calculateTotalScore の結果を変数に代入
+    console.log(scores3);
+    console.log("結果" + totalScore);
+
+    const resultContainer = document.getElementById('result-container3');
+    const resultTextElement = document.getElementById('result-text3');
+
+
+    // 結果を比較して表示を変更
+    let resultText3 = "";
+    if (totalScore <= 12) {
+        resultText3 = "青";
+    } else if (totalScore <= 16) {
+        resultText3 = "紫";
+    } else if (totalScore <= 20) {
+        resultText3 = "緑";
+    } else if (totalScore <= 24) {
+        resultText3 = "黄色";
+    } else if (totalScore <= 27) {
+        resultText3 = "オレンジ";
+    } else if (totalScore <= 30) {
+        resultText3 = "赤";
+    } else {
+        resultText3 = "その他"; // それ以外の場合にも対応する場合
+    }
+
+    resultTextElement.textContent = resultText3;
+
+    // 質問画面を非表示
+    document.getElementById('question-container3').style.display = 'none';
+    document.getElementById('answer-form3').style.display = 'none';
+    resultContainer.style.display = 'block';
+}
+
+
+function calculateTotalScore3() {
+    // 各質問の得点を合算する
+    return scores3.reduce((total, score) => total + score, 0);
 }
